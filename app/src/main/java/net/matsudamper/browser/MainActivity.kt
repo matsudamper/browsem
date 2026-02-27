544ac7487d1397deb97887dffc6b9a9f20ca4e5f
matsudamper <matsudamper@hatena.ne.jp>
2026-02-27 19:34:59 +0900

    init

---
+++ app/src/main/java/net/matsudamper/browser/MainActivity.kt
@@ -0,0 +1,40 @@
+package net.matsudamper.browser
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BrowserApp()
+        }
+    }
+}
+
+@Composable
+private fun BrowserApp() {
+    MaterialTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Box(contentAlignment = Alignment.Center) {
+                Text(text = "Hello Compose")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BrowserAppPreview() {
+    BrowserApp()
+}
