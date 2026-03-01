2f9ecfe08491fb21cad08829c38ce372c1ad5c38
matsudamper <let@matsudamper.net>
2026-03-01 15:33:47 +0900

    align fix

--- app/src/main/java/net/matsudamper/browser/BrowserToolBar.kt
+++ app/src/main/java/net/matsudamper/browser/BrowserToolBar.kt
@@ -43,6 +43,7 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
@@ -110,6 +111,7 @@ internal fun BrowserToolBar(
                 Text(
                     text = "$tabCount",
                     style = MaterialTheme.typography.titleMedium,
+                    textAlign = TextAlign.Center,
                     modifier = Modifier
                         .clip(RoundedCornerShape(8.dp))
                         .border(
