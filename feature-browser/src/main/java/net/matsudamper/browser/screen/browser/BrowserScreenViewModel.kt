c3583a927a843ff897beb0b13b6423655389f94a
matsudamper <let@matsudamper.net>
2026-03-28 11:38:38 +0900

    fix import

--- feature-browser/src/main/java/net/matsudamper/browser/screen/browser/BrowserScreenViewModel.kt
+++ feature-browser/src/main/java/net/matsudamper/browser/screen/browser/BrowserScreenViewModel.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
