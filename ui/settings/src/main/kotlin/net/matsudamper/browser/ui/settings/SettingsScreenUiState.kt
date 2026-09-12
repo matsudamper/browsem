4df01203166be6e49b2b39a566e5289c94f069a9
matsudamper <matsudamper971227@gmail.com>
2026-09-12 21:30:26 +0900

    Gemini Nano利用可否をViewModel内に限定する

--- ui/settings/src/main/kotlin/net/matsudamper/browser/ui/settings/SettingsScreenUiState.kt
+++ ui/settings/src/main/kotlin/net/matsudamper/browser/ui/settings/SettingsScreenUiState.kt
@@ -15,7 +15,6 @@ data class SettingsScreenUiState(
     val customSearchUrl: String,
     val themeMode: ThemeMode,
     val translationProvider: TranslationProvider,
-    val geminiNanoAvailable: Boolean,
     val enableThirdPartyCa: Boolean,
     val enableWebSuggestions: Boolean,
     val inputAutoZoomEnabled: Boolean,
